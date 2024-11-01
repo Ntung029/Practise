8d80131d88ffd4e3a7ad921f2046d528ad1c86e7
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-10-31 22:18:33 -0700

    Time: 24 ms (53.00%) | Memory: 16.6 MB (46.33%) - LeetSync

---
+++ 761-employee-free-time/employee-free-time.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    void mergeIntervals(vector<Interval>& s, Interval a)
+    {
+        // merge intervala to s
+    }
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        /*
+            -1 1 3 4 10 100
+        */
+
+        map<int,int> mp;
+        int N = schedule.size();
+        for (int i = 0; i<N; i++)
+        {
+            for (int j = 0; j<schedule[i].size(); j++)
+            {
+                int s = schedule[i][j].start;
+                int e = schedule[i][j].end;
+                if (mp.end() != mp.find(s))
+                {
+                    mp[s] = max(mp[s],e);
+                }
+                else
+                {
+                    mp[s] = e;
+                }
+            }
+        }
+        vector<Interval> res;
+        int prev = -1;
+        for (auto& it:mp)
+        {
+            if ((prev !=-1) && (prev < it.first))
+            {
+                res.push_back(Interval(prev,it.first));
+            }
+            prev = max(prev,it.second);
+        }
+        return res;
+    }
+};
