1f9a837fc53733961d26e9a1bfb5555bb65f032f
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-10-31 22:36:59 -0700

    Time: 0 ms (100.00%) | Memory: 22.7 MB (30.36%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+    vector<vector<int>> intervalIntersection2(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
         vector<vector<int>> result;
         int i = 0;
         int j = 0;
@@ -31,26 +31,20 @@ public:
     
     
     
-    
-    
-    
-    vector<vector<int>> intervalIntersection2(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        vector<vector<int>> result;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
         int i = 0;
-        int j = 0;    
-        // no overlap when one list is finish
+        int j = 0;
+        vector<vector<int>> result;
         while ((i<firstList.size()) && (j<secondList.size()))
         {
-            int lo = max(firstList[i][0],secondList[j][0]);
-            int hi = min(firstList[i][1],secondList[j][1]);
-
-            if (lo<=hi)
+            int left = max(firstList[i][0],secondList[j][0]);
+            int right = min(firstList[i][1],secondList[j][1]);
+            if (left<=right)
             {
-                result.push_back({lo,hi});
+                result.push_back({left,right});
             }
 
-            // move to the next interval
-            if (firstList[i][1] < secondList[j][1])
+            if (firstList[i][1]<secondList[j][1])
             {
                 i++;
             }
@@ -58,7 +52,6 @@ public:
             {
                 j++;
             }
-            
         }
         return result;
     }
