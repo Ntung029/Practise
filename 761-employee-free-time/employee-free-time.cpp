064bf0916a37ad5eabb4fb76e70cbbf73abdb751
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-10-31 22:32:25 -0700

    Time: 28 ms (37.00%) | Memory: 16.8 MB (41.87%) - LeetSync

--- 761-employee-free-time/employee-free-time.cpp
+++ 761-employee-free-time/employee-free-time.cpp
@@ -20,7 +20,42 @@ public:
     {
         // merge intervala to s
     }
-    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule)
+    {
+        map<int,int> mp;
+        int N = schedule.size();
+        for (int i = 0; i<N; i++)
+        {
+            for (int j = 0; j< schedule[i].size(); j++)
+            {
+                int s = schedule[i][j].start;
+                int e = schedule[i][j].end;
+                if (mp.end() != mp.find(s))
+                {
+                    mp[s] = max(mp[s],e);
+                }
+                else
+                {
+                    mp[s] = e;
+                }
+            }
+        }
+
+        // find the free schedule
+        vector<Interval> result;
+        int prev = -1;
+        for (const auto it:mp)
+        {
+            if ((prev !=-1) && (it.first>prev))
+            {
+                result.push_back(Interval(prev,it.first));
+            }
+            prev = max(prev,it.second);
+        }
+        return result;
+    }
+
+    vector<Interval> employeeFreeTime2(vector<vector<Interval>> schedule) {
         /*
             -1 1 3 4 10 100
         */
