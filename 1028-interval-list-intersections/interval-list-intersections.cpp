89a4e43fea84c2530346079db84699e37b185d80
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-10-31 22:26:03 -0700

    Time: 0 ms (100.00%) | Memory: 22.5 MB (64.12%) - LeetSync

---
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> result;
+        int i = 0;
+        int j = 0;
+        while ((i<firstList.size()) && (j<secondList.size()))
+        {
+            int left = max (firstList[i][0],secondList[j][0]);
+            int right = min(firstList[i][1], secondList[j][1]);
+            if (left<=right)
+            {
+                result.push_back({left,right});
+            }
+            if (firstList[i][1] > secondList[j][1])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return result;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    vector<vector<int>> intervalIntersection2(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> result;
+        int i = 0;
+        int j = 0;    
+        // no overlap when one list is finish
+        while ((i<firstList.size()) && (j<secondList.size()))
+        {
+            int lo = max(firstList[i][0],secondList[j][0]);
+            int hi = min(firstList[i][1],secondList[j][1]);
+
+            if (lo<=hi)
+            {
+                result.push_back({lo,hi});
+            }
+
+            // move to the next interval
+            if (firstList[i][1] < secondList[j][1])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+            
+        }
+        return result;
+    }
+};
