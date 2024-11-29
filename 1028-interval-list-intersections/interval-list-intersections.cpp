d8d22264be780803e898db97872d36a9bf35f1e8
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-11-29 09:51:21 -0800

    Time: 3 ms (48.25%) | Memory: 22.8 MB (23.92%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -37,14 +37,15 @@ public:
         vector<vector<int>> result;
         while ((i<firstList.size()) && (j<secondList.size()))
         {
-            int left = max(firstList[i][0],secondList[j][0]);
-            int right = min(firstList[i][1],secondList[j][1]);
+            int left = max(firstList[i][0], secondList[j][0]);
+            int right = min(firstList[i][1], secondList[j][1]);
+
             if (left<=right)
             {
                 result.push_back({left,right});
             }
 
-            if (firstList[i][1]<secondList[j][1])
+            if (firstList[i][1] < secondList[j][1])
             {
                 i++;
             }
