baa4b23d8b0530ad438a8f80fdd8a9201e989b0f
Ntung029 <Nguyenthanhtung029@gmail.com>
2024-12-22 22:31:18 -0800

    Time: 1 ms (61.08%) | Memory: 11.4 MB (34.58%) - LeetSync

---
+++ 249-group-shifted-strings/group-shifted-strings.cpp
@@ -0,0 +1,60 @@
+class Solution2 {
+public:
+    int decode(const string& s)
+    {
+        if (s.length()<2) return INT32_MIN;
+        int result = 0;
+        for (int i = 1; i<s.length(); i++)
+        {
+            result += (s[i] - s[i - 1] + 26) % 26 + 'a';
+        }
+        return result;
+    }
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        unordered_map<int,vector<string>> mp;
+        for (const auto& s:strings)
+        {
+            int i = decode(s);
+            mp[i].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto it:mp)
+        {
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    // Create a hash value
+    string getHash(string &s) {
+        string hashKey;
+        for (int i = 1; i < s.length(); i++) {
+            hashKey += (s[i] - s[i - 1] + 26) % 26 + 'a';
+        }
+        
+        return hashKey;
+    }
+    
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        unordered_map<string, vector<string>> mapHashToList;
+        
+        // Create a hash_value (hashKey) for each string and append the string
+        // to the list of hash values i.e. mapHashToList["cd"] = ["acf", "gil", "xzc"]
+        for (string str : strings) {
+            string hashKey = getHash(str);
+            mapHashToList[hashKey].push_back(str);
+        }
+        
+        // Iterate over the map, and add the values to groups
+        vector<vector<string>> groups;
+        for (auto it : mapHashToList) {
+            groups.push_back(it.second);
+        }
+        
+        // Return a list of all of the grouped strings
+        return groups;
+    }
+};
